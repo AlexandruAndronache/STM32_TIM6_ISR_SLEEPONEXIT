042db10d8b33b7098d20fa3a213ceec1dda950d7
AlexandruAndronache <alexandru.andronache21@gmail.com>
2024-08-27 17:45:48 +0300

    Project commit

---
+++ STM32_TIM6_ISR_SLEEPONEXIT/Core/Src/data.c
@@ -0,0 +1,12 @@
+/*
+ * data.c
+ *
+ *  Created on: Aug 27, 2024
+ *      Author: alexa
+ */
+
+
+#include "main.h"
+
+//data to be sent
+char dataViaUart[50] = "Hello! We are testing SLEEPONEXIT feature\r\n";
